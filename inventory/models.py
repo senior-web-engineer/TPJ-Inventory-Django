c0223c304bb2af2d799ce7e3a1bdd193da40016f
smart0120 <gadrgon0305@gmail.com>
2021-06-23 09:38:01 +0200

    feat: create tables for catalog

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Sku(models.Model):
+    sku_id = models.CharField(max_length=50)
+    sku_name = models.CharField(max_length=50)
+    product_name = models.CharField(max_length=50)
+    product_category = models.CharField(max_length=50)
+    product_description = models.CharField(max_length=255)
+    style = models.CharField(max_length=50)
+    color = models.CharField(max_length=50)
+    size = models.IntegerField()
+    available_to_sell = models.IntegerField()
+    sales_last_week = models.IntegerField()
+    sales_last_4_weeks = models.IntegerField()
+    sales_last_52_weeks = models.IntegerField()
+
+
+class Order(models.Model):
+    order_id = models.CharField(max_length=50)
+    status = models.CharField(max_length=20)
+    order_type = models.CharField(max_length=20)
+    submitted_at = models.DateTimeField()
+    sku_name = models.CharField(max_length=50)
+    quantity = models.IntegerField()
