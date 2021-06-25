8ffb1a879ce5d0cbd7ad027edfd669d11bb1b46e
smart0120 <gadrgon0305@gmail.com>
2021-06-25 15:17:25 +0200

    fix: delete collected staticfiles

--- staticfiles/js/custom.js
+++
@@ -1,5 +0,0 @@
-$(function () {
-    var current = location.pathname;
-    $('.sidebar .nav-item.active').removeClass('active');
-    $(`.sidebar .nav-link[href='${current}']`).parent('.nav-item').addClass('active');
-});
